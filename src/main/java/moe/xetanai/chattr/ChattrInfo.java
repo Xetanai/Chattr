9eadde323c0a5a141b511b6d614d092a611ac956
Julia Lynn Rogers <julia@xetanai.moe>
2018-01-23 15:00:44 -0500

    Alpha 2

--- src/main/java/moe/xetanai/chattr/ChattrInfo.java
+++ src/main/java/moe/xetanai/chattr/ChattrInfo.java
@@ -6,7 +6,7 @@ public class ChattrInfo {
 	public static final int VERSION_MAJOR = 0;
 	public static final int VERSION_MINOR = 2;
 	public static final int VERSION_PATCH = 0;
-	public static final int BUILD = 14;
+	public static final int BUILD = 15;
 	public static final String VERSION = String.format("%d.%d.%d_%d", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, BUILD);
 
 	private ChattrInfo() {}
